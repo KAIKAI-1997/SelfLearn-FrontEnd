d2195b5318d149978f018ffa3d8371c13631b4f9
kai <kaikai1997@usc.com>
2021-09-23 15:23:33 -0700

    adding rolling promo effection

--- shopping/js/index.js
+++ shopping/js/index.js
@@ -1,3 +1,121 @@
+function animate(obj, interval_time, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function() {
+        var step = (interval_time - obj.offsetLeft) / 10;
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        if (obj.offsetLeft == interval_time) {
+            clearInterval(obj.timer);
+            if (callback) {
+                callback();
+            }
+        }
+        obj.style.left = obj.offsetLeft + step + "px";
+    }, 15);
+
+}
+
+window.addEventListener('load', function() {
+    // left right disappear and show
+    var left = document.querySelector('.prev');
+    var right = document.querySelector('.after');
+    var focus = document.querySelector('.focus');
+    var areawidth = focus.offsetWidth;
+
+    focus.addEventListener('mouseenter', function() {
+        left.style.display = 'block';
+        right.style.display = 'block';
+        clearTimeout(auto_play);
+        auto_play = null;
+    })
+
+    focus.addEventListener('mouseleave', function() {
+        left.style.display = 'none';
+        right.style.display = 'none';
+        auto_play = setInterval(() => {
+            right.click();
+        }, 2000);
+    })
+
+    // circle nav
+    var nav = document.querySelector('.promo-nav');
+    var circle = nav.children;
+    var roll = document.querySelector('.show_area');
+
+
+    for (var i = 0; i < circle.length; i++) {
+        circle[i].setAttribute('data_index', i);
+        circle[i].addEventListener('click', function() {
+            for (var j = 0; j < circle.length; j++) {
+                circle[j].className = '';
+                this.className = 'current';
+            }
+            num = this.getAttribute('data_index');
+            circle_index = this.getAttribute('data_index');
+            animate(roll, -this.getAttribute('data_index') * areawidth);
+        })
+    }
+    // clone first picture
+    var first = roll.children[0].cloneNode(true);
+    roll.append(first);
+
+    // left right button
+    var num = 0;
+    var circle_index = 0;
+    var flag = true;
+    right.addEventListener('click', function() {
+        if (flag) {
+            flag = false;
+            if (num == roll.children.length - 1) {
+                roll.style.left = 0;
+                num = 0;
+            }
+            num++;
+            animate(roll, -num * areawidth, function() {
+                flag = true;
+            });
+
+            circle_index++;
+            if (circle_index == nav.children.length) {
+                circle_index = 0;
+            }
+
+            for (var i = 0; i < nav.children.length; i++) {
+                nav.children[i].className = '';
+            }
+            nav.children[circle_index].className = 'current';
+        }
+    })
+
+    left.addEventListener('click', function() {
+        if (flag) {
+            flag = false;
+            if (num == 0) {
+                roll.style.left = -(roll.children.length - 1) * areawidth + 'px';
+                num = roll.children.length - 1;
+            }
+            num--;
+            animate(roll, -num * areawidth, function() {
+                flag = true;
+            });
+
+            circle_index--;
+            if (circle_index < 0) {
+                circle_index = nav.children.length - 1;
+            }
+
+            for (var i = 0; i < nav.children.length; i++) {
+                nav.children[i].className = '';
+            }
+            nav.children[circle_index].className = 'current';
+        }
+    })
+
+    // auto play
+    var auto_play = setInterval(() => {
+        right.click();
+    }, 2000);
+})
+
 window.addEventListener('DOMContentLoaded', function() {
     var ad = document.querySelector('.load_ad');
     var hour = document.querySelector('#hour');
@@ -100,19 +218,3 @@ window.addEventListener('DOMContentLoaded', function() {
         })
     }
 });
-
-function animate(obj, interval_time, callback) {
-    clearInterval(obj.timer);
-    obj.timer = setInterval(function() {
-        var step = (interval_time - obj.offsetLeft) / 10;
-        step = step > 0 ? Math.ceil(step) : Math.floor(step);
-        if (obj.offsetLeft == interval_time) {
-            clearInterval(obj.timer);
-            if (callback) {
-                callback();
-            }
-        }
-        obj.style.left = obj.offsetLeft + step + "px";
-    }, 15);
-
-}
